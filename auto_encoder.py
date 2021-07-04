a60d7278c0ab903adfa1d12f07742cc4c6e30cb8
Sami Barchid <sami.barchid@univ-lille.fr>
2021-07-05 01:37:05 +0200

    init

---
+++ auto_encoder.py
@@ -0,0 +1,157 @@
+from typing import List, Tuple, Union
+from utils import ConvBNAct
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class AutoEncoder(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        backbone: nn.Module,
+        backbone_out: int,
+        latent_dim: Union[int, None] = None,
+        decoder_channels: List = [512, 256, 128, 64, 3],
+        decoder_strategy: str = 'upsampling',
+        **kwargs
+    ):
+        """Constructor
+
+        Args:
+            in_channels (int): Number of channels of the input image.
+            latent_dim (Union[int, None], optional): Dimension of the latent vector. If the type is 'int', then the encoder will build a 1D latent vector. If the type is None, then the encoder willd build a 2D latent tensor.
+            backbone (nn.Module): pytorch module representing the backbone network used in the encoder
+            backbone_out (int): output channels of the backbone network.
+            decoder_channels (List, optional): channel configuration of the decoder. Defaults to [512, 256, 128, 64, 3]
+            decoder_strategy (str, optional): strategy employed to upsample the decoder through the 5 blocks. Can be either 'upsampling' or 'transpose_conv'.
+        """
+        super(AutoEncoder, self).__init__()
+        self.encoder = Encoder(
+            backbone,
+            backbone_out=backbone_out,
+            latent_dim=latent_dim
+        )
+        self.decoder = Decoder(
+            in_channels=backbone_out if latent_dim is None else latent_dim,
+            channels=decoder_channels,
+            strategy=decoder_strategy
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+class Encoder(nn.Module):
+    """
+    Module that encapsulates the encoder part of an AutoEncoder model.
+    It uses a backbone network to extract a 2D conv feature map and applies a Dense layer
+    to create a latent vector. This latent vector is the final output of this module.
+    """
+
+    def __init__(
+        self,
+        backbone: nn.Module,
+        backbone_out: int = None,
+        latent_dim: int = None
+    ):
+        """Constructor
+
+        Args:
+            backbone (nn.Module): backbone network to use in the Encoder. The backbone network MUST end with a 2D conv layer.
+            latent_dim (int, optional): size of the output latent vector. If None, keeps the 2D feature maps of the backbone network as a latent tensor.
+            backbone_out (int, optional): dimension of the flattened backbone output feature map. If None, keeps the 2D feature maps of the backbone network as a latent tensor.
+        """
+        super(Encoder, self).__init__()
+        self.backbone = backbone
+
+        if latent_dim is None or backbone_out is None:
+            self.latent_module = None
+        else:
+            self.latent_module = nn.Sequential(
+                nn.Flatten(),
+                nn.Linear(in_features=backbone_out, out_features=latent_dim)
+            )
+
+    def forward(self, x):
+        x = self.backbone(x)
+
+        if self.latent_module is not None:
+            x = self.latent_module(x)
+
+        return x
+
+
+class Decoder(nn.Module):
+    """Module that represents the Decoder module"""
+
+    def __init__(
+        self,
+        in_channels: int,
+        channels: List = [512, 256, 128, 64, 3],
+        strategy: str = 'upsampling'
+    ):
+        """Constructor
+
+        Args:
+            in_channels (int): input channels
+            channels (List, optional): List of int values representing the channels for each of the 5 decoder's blocks. Defaults to [512, 256, 128, 64, 3].
+            strategy (str, optional): Strategy to upsample the feature maps through the decoder's blocks. Values available: 'upsampling' or 'transpose_conv'. Defaults to 'upsampling'.
+        """
+        super(Decoder, self).__init__()
+
+        # check params
+        if len(channels) != 5:
+            raise ValueError(
+                f'channels argument must have a length of 5. Got {channels} instead.')
+
+        if strategy not in ['upsampling', 'transpose_conv']:
+            raise ValueError(
+                f'strategy argument must be \'upsampling\' or \'transpose_conv\'. Got {strategy} instead.')
+
+        blocks = [None, None, None, None, None]
+        if strategy == 'upsampling':
+            blocks[0] = nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                ConvBNAct(channels_in=in_channels,
+                          channels_out=channels[0], kernel_size=3)
+            )
+            blocks[1] = nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                ConvBNAct(
+                    channels_in=channels[0], channels_out=channels[1], kernel_size=3)
+            )
+            blocks[2] = nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                ConvBNAct(
+                    channels_in=channels[1], channels_out=channels[2], kernel_size=3)
+            )
+            blocks[3] = nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                ConvBNAct(
+                    channels_in=channels[2], channels_out=channels[3], kernel_size=3)
+            )
+            blocks[4] = nn.Sequential(
+                nn.Upsample(scale_factor=2),
+                ConvBNAct(
+                    channels_in=channels[3], channels_out=channels[4], kernel_size=3)
+            )
+        else:
+            # TRANSPOSE CONVOLUTION STRATEGY
+            blocks[0] = ConvBNAct(
+                channels_in=in_channels, channels_out=channels[0], kernel_size=3, is_transposed=True)
+            blocks[1] = ConvBNAct(
+                channels_in=channels[0], channels_out=channels[1], kernel_size=3, is_transposed=True)
+            blocks[2] = ConvBNAct(
+                channels_in=channels[1], channels_out=channels[2], kernel_size=3, is_transposed=True)
+            blocks[3] = ConvBNAct(
+                channels_in=channels[2], channels_out=channels[3], kernel_size=3, is_transposed=True)
+            blocks[4] = ConvBNAct(
+                channels_in=channels[3], channels_out=channels[4], kernel_size=3, is_transposed=True)
+
+        self.decoder = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        x = self.decoder(x)
+        return x
---
+++ utils.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class ConvBNAct(nn.Sequential):
+    """Sequential module to implement the Conv2D+BatchNorm+Activation block
+    """
+
+    def __init__(self, channels_in, channels_out, kernel_size, activation=nn.ReLU(inplace=True), dilation=1, stride=1, use_bn=True, is_transposed=False):
+        """Constructor
+
+        Args:
+            channels_in (int): input channel number
+            channels_out (int): output channel number
+            kernel_size (int|tuple): Size the conv kernel
+            activation (any, optional): activation function. Defaults to nn.ReLU(inplace=True).
+            dilation (int, optional): dilation rate parameter of the conv layer. Defaults to 1.
+            stride (int, optional): stride parameter of the conv layer. Defaults to 1.
+            use_bn (bool, optional): flag that indicates whether a batch norm is used
+        """
+        super(ConvBNAct, self).__init__()
+        padding = kernel_size // 2 + dilation - 1
+
+        if is_transposed:
+            self.add_module('conv', nn.ConvTranspose2d(channels_in, channels_out,
+                                                       kernel_size=kernel_size,
+                                                       padding=padding,
+                                                       bias=not use_bn,  # No bias if Batch Normalization is used
+                                                       dilation=dilation,
+                                                       stride=stride))
+        else:
+            self.add_module('conv', nn.Conv2d(channels_in, channels_out,
+                                              kernel_size=kernel_size,
+                                              padding=padding,
+                                              bias=not use_bn,  # No bias if Batch Normalization is used
+                                              dilation=dilation,
+                                              stride=stride))
+        if use_bn:
+            self.add_module('bn', nn.BatchNorm2d(channels_out))
+        self.add_module('act', activation)
+
+
+def get_backbone_from_model(model, n=1):
+    """Creates a new backbone network from the original in parameter where last n layers from the original backbone are removed.
+    This function is useful to adapt a pretrained model from the torchvision.models package.
+
+    Args:
+        model (nn.Module): the base classification network that contains the last FC layer to remove
+        n (int, optional): the number of last layers that will be removed. Default to 1 (because generally, the last layer is a Dense one)
+
+    Returns:
+        nn.Module: the newly created backbone module without an FC layer as the last layer.
+    """
+    return torch.nn.Sequential(*(list(model.children())[:-n]))
