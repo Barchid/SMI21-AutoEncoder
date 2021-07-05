decaf230d0efa67c66399f75a62ab30e8c613aa2
Sami Barchid <sami.barchid@univ-lille.fr>
2021-07-05 14:07:01 +0200

    add vonenet and vonenet backbone

--- base_backbone.py
+++ base_backbone.py
@@ -1,10 +1,12 @@
 """Contains several basic backbones"""
-from typing import List
-from utils import ConvBNAct
+from typing import List, OrderedDict
+from utils import ConvBNAct, get_backbone_from_model
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from vonenet import VOneNet
+
 
 class BaseBackbone(nn.Sequential):
     def __init__(
@@ -17,10 +19,10 @@ class BaseBackbone(nn.Sequential):
         # check params
         assert len(channels) == 5 and len(kernel_sizes) == 5
 
-        # insert values to iterate 
+        # insert values to iterate
         channels.insert(0, in_channels)
         kernel_sizes.insert(0, 0)
-        
+
         for i in range(len(channels) - 1):
             self.add_module(
                 f'conv_{i}',
@@ -34,3 +36,33 @@ class BaseBackbone(nn.Sequential):
                 f'maxpool_{i}',
                 nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
             )
+
+
+class VOneNetBackbone(nn.Module):
+    """Some Information about VOneNetBackbone"""
+
+    def __init__(self,
+                 sf_corr=0.75, sf_max=9, sf_min=0, rand_param=False, gabor_seed=0,
+                 simple_channels=256, complex_channels=256,
+                 noise_mode='neuronal', noise_scale=0.35, noise_level=0.07, k_exc=25,
+                 model_arch='resnet50', image_size=224, visual_degrees=8, ksize=25, stride=4, n=2):
+        super(VOneNetBackbone, self).__init__()
+        # Get the original VOneNet classifier
+        vonenet = VOneNet(
+            sf_corr, sf_max, sf_min, rand_param, gabor_seed,
+            simple_channels, complex_channels,
+            noise_mode, noise_scale, noise_level, k_exc, model_arch, image_size, visual_degrees,
+            ksize, stride
+        )
+
+        # retrieve only the backbone (without FC layer at the end)
+        self.vonenet_backbone = nn.Sequential(OrderedDict([
+            ('vone_block', vonenet.vone_block),
+            ('bottleneck', vonenet.bottleneck),
+            ('model', get_backbone_from_model(vonenet.model, n=n)),
+        ]))
+        
+
+    def forward(self, x):
+        x = self.vonenet_backbone(x)
+        return x
---
+++ classif_debug.py
@@ -0,0 +1,67 @@
+import os
+from utils import get_backbone_from_model
+from torchvision import transforms
+from torchvision.transforms.transforms import Resize
+from base_backbone import BaseBackbone
+from auto_encoder import AutoEncoder
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+import matplotlib.pyplot as plt
+
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+transf = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    normalize,
+])
+
+backbone = get_backbone_from_model(torchvision.models.resnet18(pretrained=True), n=2)
+net = AutoEncoder(
+    in_channels=3,
+    latent_dim=None,
+    backbone=backbone,
+    backbone_out=512
+)
+net.to(device)
+traindataset = torchvision.datasets.CIFAR100(
+    'datasets/cifar', download=not os.path.exists('datasets/cifar'), transform=transf)
+trainloader = torch.utils.data.DataLoader(
+    traindataset, batch_size=8, shuffle=False)
+optimizer = optimizer = torch.optim.Adam(net.parameters(), lr=1e-2)
+criterion = nn.MSELoss()
+
+
+# loop over the dataset multiple times
+outputs = inputs = None
+for epoch in range(200):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, _ = data
+        inputs = inputs.to(device)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, inputs)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        break
+
+    print('Loss: {}'.format(running_loss))
+
+for i in range(inputs.shape[0]):
+    plt.imshow(inputs[i].cpu().permute(1, 2, 0).clamp(0, 1).numpy())
+    plt.savefig(f'input_{i}.png')
+
+    plt.imshow(outputs[i].detach().cpu().permute(1, 2, 0).clamp(0, 1).numpy())
+    plt.savefig(f'output_{i}.png')
+print('Finished Training')
