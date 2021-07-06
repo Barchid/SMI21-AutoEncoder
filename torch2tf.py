e7eea8fa3c64a4db62d1f6a381e72227c04646d7
Sami Barchid <sami.barchid@univ-lille.fr>
2021-07-06 15:30:38 +0200

    fix

--- torch2tf.py
+++ torch2tf.py
@@ -43,7 +43,7 @@ def main():
         })
 
     # from ONNX to Tensorflow file
-    os.system(f"onnx-tf convert -i tmp.onnx -o f{args.output}")
+    os.system(f"onnx-tf convert -i tmp.onnx -o {args.output}")
 
 
 if __name__ == '__main__':
