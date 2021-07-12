417e536a901c61ea3f004a7d149252942ffc7e59
Sami Barchid <sami.barchid@univ-lille.fr>
2021-07-12 21:01:38 +0200

    rmeove tqdm

--- train.py
+++ train.py
@@ -307,7 +307,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     model.train()
 
     end = time.time()
-    for i, (images, target) in enumerate(tqdm(train_loader)):
+    for i, (images, target) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
 
@@ -349,7 +349,7 @@ def validate(val_loader, model, criterion, args):
 
     with torch.no_grad():
         end = time.time()
-        for i, (images, target) in enumerate(tqdm(val_loader)):
+        for i, (images, target) in enumerate(val_loader):
             if args.gpu is not None:
                 images = images.cuda(args.gpu, non_blocking=True)
 
