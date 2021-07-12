7b599e8f83258ff328c6382e85c40885c5ea922e
Sami Barchid <sami.barchid@univ-lille.fr>
2021-07-12 20:57:15 +0200

    fix loss

--- train.py
+++ train.py
@@ -185,7 +185,7 @@ def main_worker(gpu, ngpus_per_node, args):
             model = torch.nn.DataParallel(model).cuda()
 
     # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
+    criterion = nn.MSELoss().cuda(args.gpu)
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr, momentum=0.9)
 
