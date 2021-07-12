a605f6dc9a9df4b8beeaa6c5d144ff8862ef20c7
Sami Barchid <sami.barchid@univ-lille.fr>
2021-07-12 20:39:45 +0200

    remove momentum args

--- train.py
+++ train.py
@@ -187,9 +187,7 @@ def main_worker(gpu, ngpus_per_node, args):
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(args.gpu)
 
-    optimizer = torch.optim.SGD(model.parameters(), args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
+    optimizer = torch.optim.SGD(model.parameters(), args.lr, momentum=0.9)
 
     # optionally resume from a checkpoint
     if args.resume:
